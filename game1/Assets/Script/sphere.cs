2f4fe5c5c9f1c62273b0c529ebb485e5ae3a6829
MARY <phunmung@gmail.com>
2019-10-23 16:49:11 +0900

    両方どもスクリプトをついてあげた

--- game1/Assets/Script/sphere.cs
+++ game1/Assets/Script/sphere.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class sphere : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,10 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector3(2, 2, 2);
+            transform.position = new Vector3(2, 1, 12);
         }
     }
 }
