545cbc662a862f05d10881e62bb8c50586eb088f
NELEonce <neleonce.999@gmail.com>
2019-10-24 20:03:48 +0900

    ui
    ui

---
+++ game1/Assets/Script/TestSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class TestSceneManager : MonoBehaviour
+{
+    //実験
+    void Update()
+    {
+        //スペースキーが押されたらセレクトシーンに画面遷移
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Select");    
+        }
+    }
+}
