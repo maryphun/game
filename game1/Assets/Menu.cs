d991bf7a44b0f69291fa67554cabd3d3279a7b3e
MARY <phunmung@gmail.com>
2019-10-24 19:58:42 +0900

    bolt

---
+++ game1/Assets/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UdpKit;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = "Test Match";
+
+            BoltNetwork.SetServerInfo(matchName, null);
+            BoltNetwork.LoadScene("network");
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
